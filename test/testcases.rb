d06c48f22708b316a0100086b35cfc7c11dc845f
Stuart Gale and Paul King <cronald+bishboria+nrocy@gmail.com>
2010-12-05 20:34:18 +0000

    Move all refactoring methods into their own files Add autoload script for common functions Add manual testcases Update README to credit bishboria

---
+++ test/testcases.rb
@@ -0,0 +1,57 @@
+# postconditional <leader>rcpc
+somethign if condition
+
+# inlinetemp <leader>rit
+foo = 10
+puts foo
+
+# addparameter <leader>rap
+def thingy(dependency)
+  stuff
+end
+
+# extract constant <leader>rec
+class Thing
+  def method_one
+    10
+  end
+end
+
+# extract local variable <leader>relv
+class Thing
+  def method_one
+    10
+  end
+end
+
+# rename local variable <leader>rrlv
+def method
+  asdf = 10
+end
+
+# rename instance variable <leader>rriv
+class Foo
+  def method_one
+    @bar = foo
+  end
+
+  def method_two
+    @bar = bar
+  end
+end
+
+# extract method <leader>rem
+class Foo
+  def method_one
+    @bar = foo
+  end
+
+  def method_two
+    one = 1
+    two = 2
+    three = 3
+    four = two + two
+    five = two + three
+    six = five + one
+  end
+end
