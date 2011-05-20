91ec2ecdd5c42c17574e189322d90d322de50436
Stuart Gale <bishboria@gmail.com>
2011-05-20 22:04:44 +0100

    Adding first Add Parameter feature. Need to find a way to remove the vim output from the feature output

---
+++ features/step_definitions/add_parameter_steps.rb
@@ -0,0 +1,23 @@
+Given /^I have a method with no arguments or parentheses$/ do
+  @input = <<-DOC
+def my_meth
+end
+DOC
+end
+
+When /^I add a parameter to the method$/ do
+  @commands = <<-DOC
+:normal gg
+:RAddParameter
+param
+DOC
+
+  @buffer_output = RobotVim::Runner.new.run(:input_file => @input, :commands => @commands)
+end
+
+Then /^I see the method defintion with a parameter$/ do
+  @buffer_output.should == <<-DOC
+def my_meth(param)
+end
+DOC
+end
