3907de70140f94cb623125da1c8cfe199d1ad3fd
Stuart Gale <bishboria@gmail.com>
2011-05-22 17:36:15 +0100

    Adding Extract Method feature and removing manual test case.

---
+++ features/step_definitions/extract_method_steps.rb
@@ -0,0 +1,49 @@
+Given /^I have code to extract into a new method$/ do
+  @input = <<-DOC
+class Foo
+  def method_one
+    @bar = foo
+  end
+
+  def method_two
+    one = 1
+    two = 2
+    three = 3
+    four = two + two
+    five = two + three
+    six = five + one
+  end
+end
+DOC
+end
+
+When /^I extract the code into a new method$/ do
+  @commands = <<-DOC
+:normal 11G2wv$
+:RExtractMethod
+add_them
+DOC
+end
+
+Then /^I see the code in a new method$/ do
+  result_of_executing_the_commands.should == <<-DOC
+class Foo
+  def method_one
+    @bar = foo
+  end
+
+def add_them(three,two)
+  two + three
+  end
+
+  def method_two
+    one = 1
+    two = 2
+    three = 3
+    four = two + two
+five = add_them(three,two)
+    six = five + one
+  end
+end
+DOC
+end
--- test/testcases.rb
+++ test/testcases.rb
@@ -13,19 +13,3 @@ class Foo
     @bar = bar
   end
 end
-
-# extract method <leader>rem
-class Foo
-  def method_one
-    @bar = foo
-  end
-
-  def method_two
-    one = 1
-    two = 2
-    three = 3
-    four = two + two
-    five = two + three
-    six = five + one
-  end
-end
