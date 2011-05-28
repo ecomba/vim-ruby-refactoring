1aa383e0c8ac4ae2b0163386dc0d79110c669c1e
Despo Pentara <despo@despo.me>
2011-05-28 03:25:46 +0100

    more descriptive extract method steps

--- features/step_definitions/extract_method_steps.rb
+++ features/step_definitions/extract_method_steps.rb
@@ -1,53 +1,3 @@
-Given /^I have code to extract into a new method$/ do
-  @input = <<-DOC
-class Foo
-  def method_one
-    @bar = foo
-  end
-
-  def method_two
-    one = 1
-    two = 2
-    three = 3
-    four = two + two
-    five = two + three
-    six = five + one
-  end
-end
-DOC
-end
-
-When /^I extract the code into a new method$/ do
-  @commands = <<-DOC
-:normal 11G2wv$
-:RExtractMethod
-add_them
-DOC
-end
-
-Then /^I see the code in a new method$/ do
-  result_of_executing_the_commands.should == <<-DOC
-class Foo
-  def method_one
-    @bar = foo
-  end
-
-  def add_them(three,two)
-    two + three
-  end
-
-  def method_two
-    one = 1
-    two = 2
-    three = 3
-    four = two + two
-    five = add_them(three,two)
-    six = five + one
-  end
-end
-DOC
-end
-
 When /^I select \"@commands << return_key\" and execute:$/ do |command|
   select_line
   add_to_commands command
@@ -55,5 +5,15 @@ end
 
 def select_line
   @commands = ":normal 7Gvg_"
+  add_return_key
+end
+
+When /^I select \"two \+ three\" and execute:$/ do |command|
+  select_lines
+  add_to_commands command
+end
+
+def select_lines
+  @commands = ":normal 11G2wv$"
   add_return_key
 end
